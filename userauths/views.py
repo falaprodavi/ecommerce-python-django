07ca6fa590122b1df4bbe7157b1fa1982e37ad8f
falaprodavi <falaprodavo@gmail.com>
2024-08-14 15:52:27 -0300

    feat(auth): Implement user login functionality

--- userauths/views.py
+++ userauths/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
-def register_view(request):
-    
+def register_view(request):    
     
     if request.method == 'POST':
         form = UserRegisterForm(request.POST or None)
@@ -15,8 +17,7 @@ def register_view(request):
             messages.success(request, f'Hey, {username}! You account was created successfully.')
             new_user = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
             login(request, new_user)
-            return redirect ('core:index')
-            
+            return redirect ('core:index')            
         
     else:
         form = UserRegisterForm()
@@ -26,3 +27,35 @@ def register_view(request):
     }
     
     return render(request, 'userauths/sign-up.html', context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.success(request, f"hey you are already logged in.")
+        return redirect('core:index')
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f'User with {email} does not exist')
+            
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in,")
+            return redirect ("core:index")
+        
+        else:
+            messages.warning(request, "User Does Not Exists. Create an account")
+            
+    context = {
+        
+    }
+    
+    return render(request, 'userauths/sign-in.html', context)
+        
+         
