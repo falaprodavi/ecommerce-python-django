96da8c3980d5a1787878538665b596498925be11
falaprodavi <falaprodavo@gmail.com>
2024-09-23 16:08:40 -0300

    feat(ui): Create Vendor Detail

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view
+from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view
 
 app_name = 'core'
 
@@ -10,10 +10,11 @@ urlpatterns = [
         
     # Category
     path('category/', category_list_view, name='category-list'),
-    path('category/<cid>', category_product_list_view, name='category-product-list'),
+    path('category/<cid>/', category_product_list_view, name='category-product-list'),
     
     # Vendor
     path('vendors/', vendor_list_view, name='vendor-list'),
+    path('vendor/<vid>/', vendor_detail_view, name='vendor-detail'),
     
     
     
--- core/views.py
+++ core/views.py
@@ -54,6 +54,14 @@ def vendor_list_view(request):
     vendors = Vendor.objects.all()
     context = {
         "vendors": vendors,
-    }
-    
+    }    
     return render(request, 'core/vendor-list.html', context)
+
+def vendor_detail_view(request, vid):
+    vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published").order_by("-id")
+    context = {
+        "vendor": vendor,
+        "products":products,
+    }    
+    return render(request, 'core/vendor-detail.html', context)
